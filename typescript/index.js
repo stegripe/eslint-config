c5b3804264d93dcec988c1059643919c927d7ef4
mzrtamp <mozartrafikt@gmail.com>
2023-04-09 00:34:00 +0700

    fix(typescript-eslint): fix typescript-eslint config in ESLint >=v8.35.0

--- typescript/index.js
+++ typescript/index.js
@@ -1,5 +1,6 @@
 /* eslint-env node */
 const { resolve } = require("path");
+const eslintJS = require("@eslint/js");
 const base = resolve(__dirname, "..", "index.js");
 
 // @typescript-eslint original rules
@@ -252,11 +253,7 @@ rules = {
 const baseRules = Object.entries(extensionRules).map(([key]) => [delPrefix(key), "off"]);
 
 // Import baseRules options
-const eslintRecommended = require(resolve(require.resolve("eslint"), "..", "..", "conf", "eslint-recommended.js"));
-const baseRulesOptions = {
-    ...eslintRecommended.rules,
-    ...require(base).rules
-};
+const baseRulesOptions = { ...eslintJS.configs.recommended.rules, ...require(base).rules };
 
 // Infer options from baseRulesOptions for the extensionRules
 const doneInferred = Object.entries(extensionRules)
